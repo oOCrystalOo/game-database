574a60e69f59e662e33c70ca6d5fab5626559406
Crystal Ng <ng.crystal.wy@gmail.com>
2019-06-25 23:58:29 +0000

    Worked on search feature

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -64,98 +64,83 @@ class StaticPagesController < ApplicationController
   end
   
   # Get platforms, publishers, etc available for search
-  def search 
-    require 'net/http'
-    
+  def search    
     # Get Platforms
-    platforms_total = 0
-    platforms_offset = 0
-    @platforms_data = Array.new()
-    loop do
-      response_body = get_giantbomb_api('platforms', platforms_offset)
-      platforms_total = response_body['number_of_total_results']
-      results = response_body['results']
-      platforms_offset = platforms_offset + results.length
-      results.each do |result|
-        @platforms_data << result
-      end
-      if @platforms_data.length >= platforms_total
-        break
-      end
-    end
-    
-    # Get Genres
-    genres_total = 0
-    genres_offset = 0
-    @genres_data = Array.new()
-    loop do
-      response_body = get_giantbomb_api('genres', genres_offset)
-      genres_total = response_body['number_of_total_results']
-      results = response_body['results']
-      genres_offset = genres_offset + results.length
-      results.each do |result|
-        @genres_data << result
-      end
-      if @genres_data.length >= genres_total
-        break
-      end
-    end
-    
-    # Get Themes
-    themes_total = 0
-    themes_offset = 0
-    @themes_data = Array.new()
-    loop do
-      response_body = get_giantbomb_api('themes', themes_offset)
-      themes_total = response_body['number_of_total_results']
-      results = response_body['results']
-      themes_offset = themes_offset + results.length
-      results.each do |result|
-        @themes_data << result
-      end
-      if @themes_data.length >= themes_total
-        break
-      end
-    end
+    @platforms_data = get_select_options('platforms')
   end
   
   def search_games
-    if params[:query].blank?
+    params = request.query_parameters
+    puts params.inspect
+    if params.blank?
       render plain: 'Game not found', status: :not_found
     else
+      if params["data"]
+        params = params["data"]
+      end
+      
+      filter = '';
+      offset = 0
+      params.each do |param_name, value| 
+        if param_name == 'offset'
+          offset = value
+        else
+          if filter.length > 0
+            filter = filter + ','
+          end
+          filter = filter + "#{param_name}:#{value}"
+        end
+      end
+      
+      @data = get_giantbomb_api('games', offset, filter)
+      
+      render json: @data
+    end
+  end
+  
+  def get_game_by_name
+    params = request.query_parameters
+    puts params.inspect
+    if params.blank?
+      render plain: 'Game not found', status: :not_found
+    else
+      require 'net/http'
+      
       
     end
   end
   
   private
   def get_game_data (id, get_extra_info) 
+    require 'net/http'
     if id
       uri = URI('http://www.gamespot.com/api/games/')
-        params = {
-          :api_key  => ENV['GAMESPOT_API_KEY'],
-          :format   => 'json',
-          :filter   => "id:#{id}"
-        }
-        uri.query = URI.encode_www_form(params)
-        response = Net::HTTP.get_response(uri)
-        if response.code == "301"
-          response = Net::HTTP.get_response(URI.parse(response.header['location']))
-        end
-        game_data = JSON.parse(response.body)['results'][0]
-        
-        if get_extra_info && !game_data.blank?
-          # get all resources with api
-          game_data['reviews'] = get_gamespot_api('reviews', id)
-          game_data['videos'] = get_gamespot_api('videos', id)
-          game_data['articles'] = get_gamespot_api('articles', id)
-          game_data['releases'] = get_gamespot_api('releases', id)
-        end
-        
-        return game_data
+      params = {
+        :api_key  => ENV['GAMESPOT_API_KEY'],
+        :format   => 'json',
+        :filter   => "id:#{id}"
+      }
+      uri.query = URI.encode_www_form(params)
+      response = Net::HTTP.get_response(uri)
+      if response.code == "301"
+        response = Net::HTTP.get_response(URI.parse(response.header['location']))
+      end
+      game_data = JSON.parse(response.body)['results'][0]
+
+      if get_extra_info && !game_data.blank?
+        # get all resources with api
+        game_data['reviews'] = get_gamespot_api('reviews', id)
+        game_data['videos'] = get_gamespot_api('videos', id)
+        game_data['articles'] = get_gamespot_api('articles', id)
+        game_data['releases'] = get_gamespot_api('releases', id)
+      end
+
+      return game_data
     end
   end
   
   def get_gamespot_api(type, id)
+    require 'net/http'
     if id
       uri = URI("http://www.gamespot.com/api/#{type}/")
         params = {
@@ -173,7 +158,8 @@ class StaticPagesController < ApplicationController
     end
   end
   
-  def get_giantbomb_api (type, offset)
+  def get_giantbomb_api (type, offset, filter)
+    require 'net/http'
     uri = URI("https://www.giantbomb.com/api/#{type}/")
     params = {
       :api_key  => ENV['GIANTBOMB_API_KEY'],
@@ -182,6 +168,9 @@ class StaticPagesController < ApplicationController
       :offset   => offset,
       :limit    => 100
     }
+    if filter.length > 0
+      params[:filter] = filter
+    end
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get_response(uri)
     if response.code == "301"
@@ -189,5 +178,26 @@ class StaticPagesController < ApplicationController
     end
     
     return JSON.parse(response.body)
+  end
+  
+  def get_select_options (type)
+    require 'net/http'
+    total = 0
+    offset = 0
+    data = Array.new()
+    loop do
+      response_body = get_giantbomb_api(type, offset, '')
+      total = response_body['number_of_total_results']
+      results = response_body['results']
+      offset = offset + results.length
+      results.each do |result|
+        data << result
+      end
+      if data.length >= total
+        break
+      end
+    end
+    
+    return data
   end
 end
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -34,4 +34,39 @@ RSpec.describe StaticPagesController, type: :controller do
       expect(response).to have_http_status :success
     end
   end
+  
+  describe "static_pages#search_games action" do
+    it "should throw an error when no query is entered" do
+      get :search_games
+      expect(response).to have_http_status :not_found
+    end
+    
+    it "should return the search results successfully with one filter" do
+      # Platforms ID 3 is Gameboy, Genre ID 1 is Action
+      get :search_games, params: { use_route: 'search_games/', data: { platforms: 90 } }
+      expect(response).to have_http_status :success
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value).not_to be_empty
+    end
+    
+    it "should return the search results successfully with multiple filters" do
+      # Platforms ID 3 is Gameboy, Genre ID 1 is Action
+      get :search_games, params: { use_route: 'search_games/', data: { platforms: 3, original_release_date: '|1980-12-31' } }
+      expect(response).to have_http_status :success
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value).not_to be_empty
+    end
+  end
+  
+  describe "static_pages#get_game_by_name action" do
+    it "should return page not found if no name is given" do
+      get :get_game_by_name
+      expect(response).to have_http_status :not_found
+    end
+    
+    it "should successfully get the game by name, and then redirect to the game page with id" do
+      get :get_game_by_name, params: { use_route: 'get_game_by_name/', data: { name: 'Dr. Mario' } }
+      #expect(response).to redirect_to game_path
+    end
+  end
 end
